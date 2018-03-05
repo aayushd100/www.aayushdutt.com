accbdc918db9f73fe5669721f1910c0dec33e0f9
Aayush Dutt <aayushd100@gmail.com>
2018-03-05 09:13:28 +0530

    testing actual url after removing dependency from env variable

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 //by me
-const keys = require('../config/keys');
-mongoose.connect(keys.database);
+//const keys = require('../config/keys');
+mongoose.connect("mongodb://public:qwerty123@ds163745.mlab.com:63745/aadb");
 
 
 mongoose.Promise = Promise;
